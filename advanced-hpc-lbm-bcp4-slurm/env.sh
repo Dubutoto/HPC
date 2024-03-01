7106b77012b110f1bb7e8052683556b3681612da
Geunyeong Kim <vf20093@bc4login2.bc4.acrc.priv>
2024-03-01 10:02:32 +0000

    submit

--- advanced-hpc-lbm-bcp4-slurm/env.sh
+++ advanced-hpc-lbm-bcp4-slurm/env.sh
@@ -1,2 +1,6 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+export OMP_NUM_THREADS=28
+export OMP_PROC_BIND=true
+module load languages/anaconda2/5.0.1
+module load icc/2017.1.132-GCC-5.4.0-2.26 
